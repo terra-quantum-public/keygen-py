62d21aa7996117ea5bce8222e491d8dcbb46cdb1
Cedric Kring <ck@terraquantum.swiss>
2024-10-28 09:31:12 +0100

    feat: add examples for online/offline verification

---
+++ examples/offline_verification.py
@@ -0,0 +1,23 @@
+import os
+
+from keygen_sh import KeygenConfig, set_config, verify, SchemeCode
+
+set_config(
+    KeygenConfig(
+        api_url="https://api.keygen.sh",
+        api_prefix="v1",
+        api_version="v1.7",
+        account=os.getenv("KEYGEN_ACCOUNT_ID"),
+        product=os.getenv("KEYGEN_PRODUCT_ID"),
+        license_key=os.getenv("KEYGEN_LICENSE_KEY"),
+        public_key=os.getenv("KEYGEN_PUBLIC_KEY"),
+    )
+)
+
+_LICENSE_KEY = os.getenv("OFFLINE_LICENSE_KEY")
+
+try:
+    data = verify(SchemeCode.Ed25519Sign, signed_key=_LICENSE_KEY)
+    print(f"License is valid and contains the following data {data}")
+except RuntimeError:
+    print("License could not be verified.")
---
+++ examples/online_validation.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+import uuid
+
+from keygen_sh import set_config, validate, KeygenConfig
+
+set_config(
+    KeygenConfig(
+        api_url="https://api.keygen.sh",
+        api_prefix="v1",
+        api_version="v1.7",
+        account=os.getenv("KEYGEN_ACCOUNT_ID"),
+        product=os.getenv("KEYGEN_PRODUCT_ID"),
+        license_key=os.getenv("KEYGEN_LICENSE_KEY"),
+        public_key=os.getenv("KEYGEN_PUBLIC_KEY"),
+    )
+)
+
+
+async def amain():
+    fingerprint = str(uuid.getnode())
+
+    try:
+        license = await validate(fingerprints=[fingerprint])
+        # or if the license is not bound to a specific machine identity
+        license = await validate()
+
+        print(f"License {license.id}:{license.name} is valid")
+    except RuntimeError:
+        print("License is not valid")
+
+
+if __name__ == "__main__":
+    asyncio.run(amain())
